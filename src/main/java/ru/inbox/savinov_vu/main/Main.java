284b42ca0f7ec83857bf2ba5b9523a476dfbedb7
skorpion <savinov_vu@inbox.ru>
2017-04-24 15:31:35 +0800

    get AccessToken

--- src/main/java/ru/inbox/savinov_vu/main/Main.java
+++ src/main/java/ru/inbox/savinov_vu/main/Main.java
@@ -1,11 +1,12 @@
 package ru.inbox.savinov_vu.main;
 
+import com.restfb.Connection;
 import com.restfb.DefaultFacebookClient;
 import com.restfb.FacebookClient;
 import com.restfb.Version;
-import com.restfb.types.User;
+import com.restfb.types.Post;
 
-import java.util.ResourceBundle;
+import java.util.List;
 
 public class Main {
 
@@ -13,10 +14,18 @@ public class Main {
    String accessToken = "EAATZC6PZA3CgoBAOG66cJdtzZADHgT7rF0OWZCGHgAUAXEUlb6MD2prTftvQk4wzOKO5ZAVJI3ZAs42PAxtaaKNfZCJw8t4mwUg3tV0xsgN8ZCHdKP4WrEnrugyqji7jkigYqnmOZCEIDYZApG66Tr9aY1IKelf0U6JZB4ZD";
         FacebookClient fbclient = new DefaultFacebookClient(accessToken, Version.LATEST);
 
-        FacebookClient.AccessToken at = fbclient.obtainExtendedAccessToken("1407275939334666", ResourceBundle.getBundle("config").getString("secret"));
-        User me = fbclient.fetchObject("me", User.class);
-        System.out.println(me.getName());
-        System.out.println(at.getAccessToken());
-        System.out.println(at.getExpires());
+        Connection<Post> result = fbclient.fetchConnection("me/feed", Post.class);
+        int counter = 0;
+
+
+        for (List<Post> page : result) {
+            for (Post post : page) {
+                System.out.println(post.getMessage());
+                System.out.println("fb.com/" + post.getId());
+                counter++;
+            }
+        }
+        System.out.println(counter);
+
     }
 }
