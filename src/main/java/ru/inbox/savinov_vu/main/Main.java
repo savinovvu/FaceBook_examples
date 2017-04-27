c9423e7e9fc739399cc3e22622328050b82d5d4d
skorpion <savinov_vu@inbox.ru>
2017-04-27 09:50:17 +0800

    get AccessToken

--- src/main/java/ru/inbox/savinov_vu/main/Main.java
+++ src/main/java/ru/inbox/savinov_vu/main/Main.java
@@ -1,31 +1,25 @@
 package ru.inbox.savinov_vu.main;
 
-import com.restfb.Connection;
-import com.restfb.DefaultFacebookClient;
-import com.restfb.FacebookClient;
-import com.restfb.Version;
-import com.restfb.types.Post;
-
-import java.util.List;
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
 
     public static void main(String[] args) {
-   String accessToken = "EAATZC6PZA3CgoBAOG66cJdtzZADHgT7rF0OWZCGHgAUAXEUlb6MD2prTftvQk4wzOKO5ZAVJI3ZAs42PAxtaaKNfZCJw8t4mwUg3tV0xsgN8ZCHdKP4WrEnrugyqji7jkigYqnmOZCEIDYZApG66Tr9aY1IKelf0U6JZB4ZD";
-        FacebookClient fbclient = new DefaultFacebookClient(accessToken, Version.LATEST);
+        String authUrl = "https://www.facebook.com/?sk=h_chr";
+        ChromeDriverManager.getInstance().setup();
 
-        Connection<Post> result = fbclient.fetchConnection("me/feed", Post.class);
-        int counter = 0;
+        WebDriver driver = new ChromeDriver();
+        driver.get(authUrl);
+        while (true) {
 
-
-        for (List<Post> page : result) {
-            for (Post post : page) {
-                System.out.println(post.getMessage());
-                System.out.println("fb.com/" + post.getId());
-                counter++;
+            if (driver.getCurrentUrl().contains("sk=h_nor")) {
+                driver.get(authUrl);
             }
         }
-        System.out.println(counter);
+
+
 
     }
 }
